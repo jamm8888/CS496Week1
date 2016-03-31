dd1103273e5dc4217a33f591493d574658785ebc
Jamm8888 <ammj@oregonstate.edu>
2016-03-31 16:10:19 -0700

    adding django.py

---
+++ django.py
@@ -0,0 +1,11 @@
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+
+	def get(self):
+		self.response.write("This is django dir")
+		print 'this is your guess'
+
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+], debug=True)

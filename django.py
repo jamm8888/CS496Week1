d54f06027fbecda447b8959add49d589356a95f0
Jamm8888 <ammj@oregonstate.edu>
2016-03-31 18:13:06 -0700

    Revert "trying again"
    This reverts commit af7ed44870f81161d8cf5213702ca2b578882b80.

--- django.py
+++ django.py
@@ -4,8 +4,8 @@ class MainPage(webapp2.RequestHandler):
 
 	def get(self):
 		self.response.write("This is django dir")
-		#print 'this is your guess'
+		print 'this is your guess'
 
 app = webapp2.WSGIApplication([
-	('/django', MainPage),
+	('/', MainPage),
 ], debug=True)

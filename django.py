a86a14fe21ebf1d995e0fe532342fd3acb6b4cbe
Jamm8888 <ammj@oregonstate.edu>
2016-03-31 18:21:43 -0700

    updates to django

--- django.py
+++ django.py
@@ -1,11 +1,11 @@
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
+class DjangoPage(webapp2.RequestHandler):
 
 	def get(self):
 		self.response.write("This is django dir")
-		print 'this is your guess'
+		#print 'this is your guess'
 
 app = webapp2.WSGIApplication([
-	('/', MainPage),
+	('/django', DjangoPage),
 ], debug=True)
